9c0a140128063a92a21b84aafe10a0fee588dfd0
Landon Abney <Landonabney@gmail.com>
2017-08-25 23:34:20 -0700

    Rewrite to use mainline CSSLint
    Rewrite the provider to use mainline csslint instead of the custom fork.
    
    Includes:
    * BREAKING: Disabling of linting on changes
    * Updating to Linter v2 API
    * Allowing users to specify the path to CSSLint. Uses project local
    first, bundled second if not specified.

--- spec/linter-csslint-spec.js
+++ spec/linter-csslint-spec.js
@@ -3,6 +3,10 @@
 import * as path from 'path';
 // eslint-disable-next-line no-unused-vars
 import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
+import linterCsslint from '../lib/main';
+
+const linterProvider = linterCsslint.provideLinter();
+const lint = linterProvider.lint;
 
 const badPath = path.join(__dirname, 'fixtures', 'bad.css');
 const goodPath = path.join(__dirname, 'fixtures', 'good.css');
@@ -11,9 +15,11 @@ const emptyPath = path.join(__dirname, 'fixtures', 'empty.css');
 const projectPath = path.join(__dirname, 'fixtures', 'project');
 const projectBadPath = path.join(projectPath, 'files', 'badWC.css');
 
-describe('The csslint provider for Linter', () => {
-  const lint = require('../lib/main.js').provideLinter().lint;
+const emptyRulesDetails = 'Rules without any properties specified should be ' +
+  'removed. (empty-rules)';
+const emptRulesUrl = 'https://github.com/CSSLint/csslint/wiki/Disallow-empty-rules';
 
+describe('The CSSLint provider for Linter', () => {
   beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
 
@@ -27,10 +33,12 @@ describe('The csslint provider for Linter', () => {
       const messages = await lint(editor);
 
       expect(messages.length).toBe(1);
-      expect(messages[0].type).toBe('Warning');
-      expect(messages[0].text).toBe('Rule is empty.');
-      expect(messages[0].filePath).toBe(badPath);
-      expect(messages[0].range).toEqual([[0, 0], [0, 4]]);
+      expect(messages[0].severity).toBe('warning');
+      expect(messages[0].excerpt).toBe('Rule is empty.');
+      expect(messages[0].details).toBe(emptyRulesDetails);
+      expect(messages[0].url).toBe(emptRulesUrl);
+      expect(messages[0].location.file).toBe(badPath);
+      expect(messages[0].location.position).toEqual([[0, 0], [0, 4]]);
     });
   });
 
@@ -38,12 +46,15 @@ describe('The csslint provider for Linter', () => {
     it('verifies the message', async () => {
       const editor = await atom.workspace.open(invalidPath);
       const messages = await lint(editor);
+      const details = 'This rule looks for recoverable syntax errors. (errors)';
 
       expect(messages.length).toBe(1);
-      expect(messages[0].type).toBe('Error');
-      expect(messages[0].text).toBe('Unexpected token \'}\' at line 1, col 1.');
-      expect(messages[0].filePath).toBe(invalidPath);
-      expect(messages[0].range).toEqual([[0, 0], [0, 1]]);
+      expect(messages[0].severity).toBe('error');
+      expect(messages[0].excerpt).toBe('Unexpected token \'}\' at line 1, col 1.');
+      expect(messages[0].details).toBe(details);
+      expect(messages[0].url).not.toBeDefined();
+      expect(messages[0].location.file).toBe(invalidPath);
+      expect(messages[0].location.position).toEqual([[0, 0], [0, 1]]);
     });
   });
 
@@ -51,14 +62,14 @@ describe('The csslint provider for Linter', () => {
     const editor = await atom.workspace.open(goodPath);
     const messages = await lint(editor);
 
-    expect(messages.length).toEqual(0);
+    expect(messages.length).toBe(0);
   });
 
   it('handles an empty file', async () => {
     const editor = await atom.workspace.open(emptyPath);
     const messages = await lint(editor);
 
-    expect(messages.length).toEqual(0);
+    expect(messages.length).toBe(0);
   });
 
   it('respects .csslintrc configurations at the project root', async () => {
@@ -67,9 +78,40 @@ describe('The csslint provider for Linter', () => {
     const messages = await lint(editor);
 
     expect(messages.length).toBe(1);
-    expect(messages[0].type).toBeDefined();
-    expect(messages[0].type).toEqual('Error');
-    expect(messages[0].text).toBeDefined();
-    expect(messages[0].text).toEqual('Rule is empty.');
+    expect(messages[0].severity).toBe('error');
+    expect(messages[0].excerpt).toBe('Rule is empty.');
+    expect(messages[0].details).toBe(emptyRulesDetails);
+    expect(messages[0].url).toBe(emptRulesUrl);
+    expect(messages[0].location.file).toBe(projectBadPath);
+    expect(messages[0].location.position).toEqual([[0, 0], [0, 4]]);
+  });
+
+  describe('dynamically determines where to execute from', () => {
+    it('uses the bundled version when unspecified and no local version', () => {
+      const packagePath = atom.packages.resolvePackagePath('linter-csslint');
+      let relativeBinPath = path.join('node_modules', '.bin', 'csslint');
+      if (process.platform === 'win32') {
+        relativeBinPath += '.cmd';
+      }
+
+      const foundPath = linterCsslint.determineExecPath('', '');
+      expect(foundPath).toBe(path.join(packagePath, relativeBinPath));
+    });
+
+    it('finds a local install if it exists', () => {
+      const execProjectPath = path.join(__dirname, 'fixtures', 'execProject');
+      let relativeBinPath = path.join('node_modules', '.bin', 'csslint');
+      if (process.platform === 'win32') {
+        relativeBinPath += '.cmd';
+      }
+
+      const foundPath = linterCsslint.determineExecPath('', execProjectPath);
+      expect(foundPath).toBe(path.join(execProjectPath, relativeBinPath));
+    });
+
+    it('trusts what the user tells it', async () => {
+      const foundPath = linterCsslint.determineExecPath('foobar', '');
+      expect(foundPath).toBe('foobar');
+    });
   });
 });
